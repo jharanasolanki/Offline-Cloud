b0a68adc1a08415b093c37c945296a5b75669571
EshaSanghavi <eshacsanghavi@gmail.com>
2022-03-27 02:38:43 +0530

    comit

--- ver1.0/scripts/test.sh
+++ ver1.0/scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # python3 -m http.server
-$2 = 'dev/sdb1';
-$3 = 'AA9D-0AFE';
+$2 = 'dev/sdc1';
+$3 = 'B35E-67E1';
 mkdir -m 777 /var/www/html/WebUSB/disk/$3
 chmod -m 0777 /var/www/html/WebUSB/disk/
-mount $2' /var/www/html/WebUSB/disk/AA9D-0AFE'
+mount $2' /var/www/html/WebUSB/disk/B35E-67E1'
