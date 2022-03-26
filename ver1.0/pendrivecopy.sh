ffe7b25c292ae74ac3976cca7088fcc8885b4e68
jharanasolanki <jharanasolanki@gmail.com>
2022-03-27 00:03:23 +0530

    pendrive copy files

---
+++ ver1.0/pendrivecopy.sh
@@ -0,0 +1,7 @@
+<?php
+$path    = "/forpendrive/";
+$files = scandir($path);
+$files = array_diff(scandir($path), array('.', '..'));
+foreach($files as $file){
+  echo "<a href='$file'>$file</a>";
+}
